bdb678f844281837c7d4c54ba71fe4b90f1c050c
sara-p- <sarapitt88@gmail.com>
2025-01-31 17:23:09 -0500

    styling the select filter.

--- src/components/FormElements/SelectField/SelectField.tsx
+++ src/components/FormElements/SelectField/SelectField.tsx
@@ -1,33 +1,44 @@
-import React from 'react'
-import { Select } from '@radix-ui/react-select'
-import styles from './SelectField.module.css'
+'use client'
 
-const SelectField: React.FC = () => {
+import React from 'react'
+import * as Select from '@radix-ui/react-select'
+import styles from './SelectField.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+type SelectFieldProps = {
+  options: { label: string; value: string }[]
+  label: string
+  onChange?: (value: string) => void
+}
+
+export default function SelectField({ options, label, onChange }: SelectFieldProps) {
+  const [value, setValue] = React.useState('')
+
+  function handleChange(value: string) {
+    setValue(value)
+    onChange?.(value)
+  }
+
   return (
-    <Select.Root>
-      <Select.Trigger>
-        <Select.Value />
-        <Select.Icon />
+    <Select.Root value={value} onValueChange={handleChange}>
+      <Select.Trigger className={styles.trigger}>
+        <Select.Value placeholder={label} />
+        <Select.Icon>
+          <FontAwesomeIcon icon={faChevronDown} />
+        </Select.Icon>
       </Select.Trigger>
 
       <Select.Portal>
-        <Select.Content>
+        <Select.Content className={styles.content}>
           <Select.ScrollUpButton />
           <Select.Viewport>
-            <Select.Item>
-              <Select.ItemText />
-              <Select.ItemIndicator />
-            </Select.Item>
-
-            <Select.Group>
-              <Select.Label />
-              <Select.Item>
-                <Select.ItemText />
+            {options.map((option, index) => (
+              <Select.Item key={index} value={option.value} className={styles.item}>
+                <Select.ItemText>{option.label}</Select.ItemText>
                 <Select.ItemIndicator />
               </Select.Item>
-            </Select.Group>
-
-            <Select.Separator />
+            ))}
           </Select.Viewport>
           <Select.ScrollDownButton />
           <Select.Arrow />
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,6 +5,8 @@ import Input from '@/components/FormElements/Input/Input'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import SidebarHeader from './SidebarHeader/SidebarHeader'
+import SelectField from '@/components/FormElements/SelectField/SelectField'
+
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
@@ -15,6 +17,17 @@ export default function Sidebar() {
             label="Search"
             placeholder="Search"
             icon={<FontAwesomeIcon icon={faSearch} style={{ width: '14px' }} />}
+          />
+        </Fieldset>
+        <Fieldset title="sort">
+          <SelectField
+            label="series"
+            placeholder="series"
+            options={[
+              { label: 'series', value: 'series' },
+              { label: 'rating', value: 'rating' },
+              { label: 'popularity', value: 'popularity' },
+            ]}
           />
         </Fieldset>
       </div>
